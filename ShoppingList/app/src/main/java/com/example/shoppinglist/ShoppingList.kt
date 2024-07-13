806e3c648d87584cfadc2cfdc23f23498879144d
tungpham.bh@gmail.com <tungpham.bh@gmail.com>
2024-07-13 11:30:09 +0700

    update ShoppingList

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,2 +1,105 @@
 package com.example.shoppinglist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class ShoppingItem(
+    val id:Int,
+    var name:String,
+    var quantity:Int,
+    val isEditing:Boolean = false
+)
+
+@Composable
+fun ShoppingList() {
+    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember{ mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQty by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Button(onClick = { showDialog = true}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Text(text = "Add Item")
+        }
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)){
+            items(sItems){
+
+            }
+        }
+    }
+
+    if (showDialog){
+        AlertDialog(onDismissRequest = {showDialog = false},
+            confirmButton = {
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween) {
+                                Button(onClick = {
+                                    if(itemName.isNotBlank()){
+                                        val sItem = ShoppingItem(
+                                            id = sItems.size + 1,
+                                            name = itemName,
+                                            quantity = itemQty.toIntOrNull() ?: 0)
+                                        sItems = sItems + sItem
+                                        showDialog = false
+                                        itemName = ""
+                                        itemQty = ""
+                                    }
+                                }) {
+                                    Text(text = "Add Item")
+                                }
+                                Button(onClick = {
+                                    showDialog = false
+                                    itemName = ""
+                                    itemQty = ""
+                                }) {
+                                    Text(text = "Cancel")
+                                }
+                            }
+            },
+            title = { Text(text = "Add Shopping Item")},
+            text = {
+                Column {
+                    OutlinedTextField(value = itemName ,
+                        onValueChange = {itemName = it},
+                        singleLine = true,
+                        modifier = Modifier.padding(8.dp),
+                        label = { Text(text = "Enter a name")}
+                    )
+
+                    OutlinedTextField(value = itemQty ,
+                        onValueChange = {itemQty = it},
+                        singleLine = true,
+                        modifier = Modifier.padding(8.dp),
+                        label = { Text(text = "Enter a quantity")}
+                    )
+                }
+            }
+        )
+
+    }
+}
